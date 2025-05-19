4d5c7826f5b7740e10738b3c5741abbdaadc3072
Midnight17Runner <laughingassassin07@gmail.com>
2025-05-19 17:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,8 @@ def article(name):
     except:
         return "Article not found", 404
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
